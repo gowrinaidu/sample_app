3f3d8ea1f3a62f7808c79f621b5ee3469a3cd7e1
gowrinaidu <gowri.naidu@hiddenbrains.in>
2012-10-16 16:20:59 +0530

    add files into repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2ac90e459b8f88e0f6b2eaa024b78fd5daa49a9b0437a704dfcede87287bfd8ccf2afa881b5df7fed2502ae992d7e0d5b5c22833a886b7668fb1d2bc3094e03f'
